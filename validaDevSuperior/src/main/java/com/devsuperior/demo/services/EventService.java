87ea701f4c6381fb6b76d3516d1cf24a709ab6a1
Ricardo Ferreira <rnhc1000@hotmail>
2024-12-15 16:32:55 -0300

    [chore] 🔧: JPA saveAndFlush return the entity and so the acess to the id cretaed for that event

--- validaDevSuperior/src/main/java/com/devsuperior/demo/services/EventService.java
+++ validaDevSuperior/src/main/java/com/devsuperior/demo/services/EventService.java
@@ -9,32 +9,21 @@ import com.devsuperior.demo.repositories.EventRepository;
 import com.devsuperior.demo.services.exceptions.ConstraintViolationException;
 import com.devsuperior.demo.utils.DtoToEntity;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-
-import static java.util.Objects.isNull;
-
 @Service
 public class EventService {
 
-  public EventService(EventRepository eventRepository, CityRepository cityRepository, CityService cityService, DtoToEntity dtoToEntity) {
+  public EventService(EventRepository eventRepository, CityService cityService) {
     this.eventRepository = eventRepository;
-    this.cityRepository = cityRepository;
     this.cityService = cityService;
-    this.dtoToEntity = dtoToEntity;
   }
 
   private final EventRepository eventRepository;
 
-  private final CityRepository cityRepository;
-
   private final CityService cityService;
-
-  private final DtoToEntity dtoToEntity;
 
   public Page<EventCityDTO> getEventsByCity(Pageable pageable) {
 
@@ -49,29 +38,9 @@ public class EventService {
 
   @Transactional
   public EventDTO registerEvent(EventDTO eventDTO) {
-
-//    if (eventDTO.getName().isEmpty()) {
-//      throw new ConstraintViolationException("Event can not be empty");
-//    }
-//    if (eventDTO.getCityId() == null) {
-//      throw new ConstraintViolationException("City can not be null");
-//    }
-//
-//    LocalDate today = LocalDate.now();
-//    LocalDate eventDate = eventDTO.getDate();
-//
-//    if (eventDate.isBefore(today)) {
-//      throw new ConstraintViolationException("No events in the past");
-//    }
-//
-//    if (isNull(eventDTO.getCityId())) {
-//      throw new ConstraintViolationException("Define a City for an Event");
-//    }
-
-
     if (cityService.validateCityId(eventDTO.getCityId()).isPresent()) {
 
-      Event event =  new Event();
+      Event event = new Event();
       City city = new City();
       city.setId(eventDTO.getCityId());
       event.setId(null);
@@ -79,15 +48,15 @@ public class EventService {
       event.setDate(eventDTO.getDate());
       event.setUrl(eventDTO.getUrl());
       event.setCity(city);
-//      event.setCity(cityRepository.getReferenceById(eventDTO.getCityId()));
-      event = eventRepository.save(event);
-      System.out.println(event);
+      event = eventRepository.saveAndFlush(event);
 
-//          eventRepository.saveEvent(eventDTO.getName(), eventDTO.getDate(), eventDTO.getUrl(), eventDTO.getCity_id());
-      return new EventDTO(event);
+      Long idEvent = event.getId();
+      eventDTO.setId(idEvent);
+
+      return eventDTO;
     } else {
       throw new ConstraintViolationException("City is not registered");
     }
 
-    }
   }
+}
