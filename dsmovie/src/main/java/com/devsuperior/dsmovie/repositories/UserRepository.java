277a13402d871fd0571d307e41747e9b0e89b2c3
Ricardo Ferreira <rnhc1000@hotmail>
2025-01-12 12:05:30 -0300

    [docs] 📚: add README.md

--- dsmovie/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
+++ dsmovie/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -1,24 +1,26 @@
 package com.devsuperior.dsmovie.repositories;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.devsuperior.dsmovie.entities.UserEntity;
-import com.devsuperior.dsmovie.projections.UserDetailsProjection;
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-	Optional<UserEntity> findByUsername(String username);
-	
-	@Query(nativeQuery = true, value = """
-			SELECT tb_user.username AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
-			FROM tb_user
-			INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
-			INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
-			WHERE tb_user.username = :username
-		""")
-	List<UserDetailsProjection> searchUserAndRolesByUsername(String username);
+  Optional<UserEntity> findByUsername(String username);
+
+  @Query(
+      nativeQuery = true,
+      value =
+          	"""
+          	SELECT tb_user.username AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
+          	FROM tb_user
+          	INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
+          	INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
+          	WHERE tb_user.username = :username
+          """)
+  List<UserDetailsProjection> searchUserAndRolesByUsername(String username);
 }
